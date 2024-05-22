92ad43adb9b8596999a01711f6abd2ae00e69fa9
sua <suhyun2726@hanmail.net>
2024-05-22 10:31:58 +0900

    동영상 갤러리 기본 동작 완성

---
+++ 04-popup/03-유튜브 동영상 갤러리/01-기본/js/script.js
@@ -0,0 +1,36 @@
+$(function () {
+  // 대상을 변수에 저장
+  const $dim = $('.dim');
+  const $videoWrap = $('.video-wrap');
+  const $video = $videoWrap.find('.video iframe');
+  const $btnClose = $('.btn-close');
+  const $selectVideo = $('.video-list > li');
+
+  // 비디오 리스트를 클릭했을 때
+  $selectVideo.on('click', function () {
+    // 선택한 비디오 링크를 받아서 변수에 저장
+    const videoLink = $(this).data('link'); /* data-link */
+    console.log(videoLink);
+
+    // $video의 src 값으로 비디오 링크를 세팅
+    $video.attr('src', videoLink);
+
+    // 선택한 비디오의 텍스트를 변수에 저장
+    const videoText = $(this).text();
+    // .caption에 세팅
+    $('.caption').text(videoText);
+
+    // dim을 보이게
+    $dim.fadeIn();
+    // videoWrap을 보이게
+    $videoWrap.addClass('active');
+  });
+
+  // 닫기버튼을 클릭했을 때
+  $btnClose.on('click', function () {
+    // dim을 안 보이게
+    $dim.fadeOut();
+    // $videoWrap을 안 보이게
+    $videoWrap.removeClass('active');
+  });
+});
